36b00526b339b5a110210bbb450a793648a6ec51
vijay kumar <vishaala999111@gmail.com>
2025-08-20 20:48:31 +0530

    profile-route

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -104,7 +104,8 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production", // only https in prod
-      sameSite: "strict",
+      sameSite: "none",
+
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
     // Successful login
